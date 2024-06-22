eb73df438bc000f240a06814818481ab3bf2ea60
Matheus Gongo <math-yami@hotmail.com>
2024-06-22 07:24:53 -0300

    rename

--- components/navbar.js
+++ components/navbar.js
@@ -69,13 +69,7 @@ export default function Navbar() {
         {/* menu  */}
         <div className="hidden text-center lg:flex lg:items-center">
           <ul className="items-center justify-end flex-1 pt-6 list-none lg:pt-0 lg:flex">
-            {navigation.map((menu, index) => (
-              <li className="mr-3 nav__item" key={index}>
-                <Link href="/" className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-[#0966AF] focus:text-[#0966AF] focus:bg-blue-100 focus:outline-none">
-                    {menu}
-                </Link>
-              </li>
-            ))}
+
           </ul>
         </div>
 
